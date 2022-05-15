70429394cfbb776d7adb9927248c7ec5a6fda795
jogomez97 <juan.gomez.2015@salleurl.edu>
2022-05-15 22:22:33 +0200

    Update run script

--- bin/run.sh
+++ bin/run.sh
@@ -12,15 +12,23 @@ fi
 export PYTHONPATH=../src/.
 
 echo "Starting model training"
-echo "Run 1/5"
-python ${PYTHONPATH}/train_mobilenet.py -p 'D:/La Salle/Xavier Sevillano Domínguez - TFM Juan Gómez/Dataset espectogrames/birds_spec_224_224_1s_v2'
-echo "Run 2/5"
-python ${PYTHONPATH}/train_mobilenet.py -p 'D:/La Salle/Xavier Sevillano Domínguez - TFM Juan Gómez/Dataset espectogrames/birds_spec_224_224_1s_v2' -s 41
-echo "Run 3/5"
-python ${PYTHONPATH}/train_mobilenet.py -p 'D:/La Salle/Xavier Sevillano Domínguez - TFM Juan Gómez/Dataset espectogrames/birds_spec_224_224_1s_v2' -s 1024
-echo "Run 4/5"
-python ${PYTHONPATH}/train_mobilenet.py -p 'D:/La Salle/Xavier Sevillano Domínguez - TFM Juan Gómez/Dataset espectogrames/birds_spec_224_224_1s_v2' -s 777
-echo "Run 5/5"
-python ${PYTHONPATH}/train_mobilenet.py -p 'D:/La Salle/Xavier Sevillano Domínguez - TFM Juan Gómez/Dataset espectogrames/birds_spec_224_224_1s_v2' -s 73059
+
+models=( "mobilenetv2" "resnet50" "vgg16")
+
+for i in "${models[@]}"
+do
+    echo "Training $i"
+    echo "Run 1/5 of $i"
+    python ${PYTHONPATH}/train.py -p 'D:/La Salle/Xavier Sevillano Domínguez - TFM Juan Gómez/Dataset espectogrames/birds_spec_224_224_1s_v2' -m $i
+    echo "Run 2/5 of $i"
+    python ${PYTHONPATH}/train.py -p 'D:/La Salle/Xavier Sevillano Domínguez - TFM Juan Gómez/Dataset espectogrames/birds_spec_224_224_1s_v2' -s 41 -m $i
+    echo "Run 3/5 of $i"
+    python ${PYTHONPATH}/train.py -p 'D:/La Salle/Xavier Sevillano Domínguez - TFM Juan Gómez/Dataset espectogrames/birds_spec_224_224_1s_v2' -s 1024 -m $i
+    echo "Run 4/5 of $i"
+    python ${PYTHONPATH}/train.py -p 'D:/La Salle/Xavier Sevillano Domínguez - TFM Juan Gómez/Dataset espectogrames/birds_spec_224_224_1s_v2' -s 777 -m $i
+    echo "Run 5/5 of $i"
+    python ${PYTHONPATH}/train.py -p 'D:/La Salle/Xavier Sevillano Domínguez - TFM Juan Gómez/Dataset espectogrames/birds_spec_224_224_1s_v2' -s 73059 -m $i
+done
+
 
 echo "Training done"
